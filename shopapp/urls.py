b4e5a9c436903ee44ec0457f418bcba29eda7bee
inncent <innokentiykim90@gmail.com>
2025-03-14 19:11:22 +0500

    Auth login_view added

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (GroupsListView, ShopIndexView, ProductDetailsView, ProductsListView, OrdersListView, create_product,
-                    create_order, OrderDetailView, ProductCreateView, ProductUpdateView)
+                    create_order, OrderDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView)
 
 app_name = 'shopapp'
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('products/create/', ProductCreateView.as_view(), name='products_create'),
     path('products/<int:pk>/', ProductDetailsView.as_view(), name='product_details'),
     path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('orders/', OrdersListView.as_view(), name='orders_list'),
     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_details'),
     path('orders/create/', create_order, name='orders_create'),
