5d598e391a7e46dc01596fa325fc21a45f58f09b
inncent <innokentiykim90@gmail.com>
2025-03-19 23:00:41 +0500

    FileField and ImageField added

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+def product_preview_dir_path(instance: "Product", filename: str) -> str:
+    return f"products/product_{instance.pk}/preview/{filename}"
+
 class Product(models.Model):
     class Meta:
         ordering = ['name', ]
@@ -10,6 +13,7 @@ class Product(models.Model):
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
+    preview = models.ImageField(null=True, blank=True, upload_to=product_preview_dir_path)
 
     def __str__(self):
         return f'Product {self.name!r}'
@@ -21,3 +25,4 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     products = models.ManyToManyField(Product, related_name='orders')
+    receipt = models.FileField(null=True, upload_to='orders/receipts/')
