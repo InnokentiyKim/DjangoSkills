ca910a434f3c8a7f73e2292b2496470c7f19cc8d
inncent <innokentiykim90@gmail.com>
2025-04-20 00:04:15 +0500

    ProductImage model added

--- shopapp/models.py
+++ shopapp/models.py
@@ -18,6 +18,14 @@ class Product(models.Model):
     def __str__(self):
         return f'Product {self.name!r}'
 
+def product_image_dir_path(instance: "ProductImage", filename: str) -> str:
+    return f"products/product_{instance.product.pk}/images/{filename}"
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to=product_image_dir_path)
+    description = models.CharField(max_length=200, null=False, blank=True)
+
 
 class Order(models.Model):
     delivery_address = models.TextField(null=False, blank=True)
